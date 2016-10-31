36ac6a3f2a5cf6e7bd7847bb9cc3197faa237081
Wang Jie <doudou19758@gmail.com>
2016-10-31 21:39:34 +0800

    first blood

---
+++ README.md
@@ -0,0 +1,21 @@
+URL2io node.js SDK
+======
+
+> Author : Wang Jie <i@i8e.net>
+
+### 官方文档
+
+<http://www.url2io.com/docs>
+
+### 使用说明
+
+#### npm安装
+    npm install cnwangjie/url2io
+
+#### 使用
+
+```js
+var url2io = require('url2io');
+url2io(token, url, fields, callback);
+// 回调函数只有一个参数即服务器返回的json所构成的对象
+```
---
+++ index.js
@@ -0,0 +1,18 @@
+var request = require('request');
+
+var url2io = function(token, url, fields, callback) {
+    request({
+        method: 'GET',
+        uri: 'http://api.url2io.com/article',
+        qs: {
+            token: token,
+            url: url,
+            fields: fields.join(',')
+        }
+    },
+    function(error, res, body) {
+        callback(JSON.parse(body));
+    });
+}
+
+module.exports = url2io;
