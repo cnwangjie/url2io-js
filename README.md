e01481afcc41f846ce0e734fe5d23c7ad9d8df8c
Wang Jie <doudou19758@gmail.com>
2016-11-01 17:34:19 +0800

    slight change

--- README.md
+++ README.md
@@ -16,6 +16,10 @@ URL2io node.js SDK
 
 ```js
 var url2io = require('url2io');
-url2io(token, url, fields, callback);
+url2io(token, url, callback, fields);
 // 回调函数只有一个参数即服务器返回的json所构成的对象
+
+var t = new url2io(token);
+t.handle(url, callback, fields);
+// 实例化后可以使用该对象的handle方法进行处理
 ```
--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 var request = require('request');
 
-var url2io = function(token, url, fields, callback) {
-    request({
-        method: 'GET',
-        uri: 'http://api.url2io.com/article',
-        qs: {
-            token: token,
-            url: url,
-            fields: fields.join(',')
-        }
-    },
-    function(error, res, body) {
-        callback(JSON.parse(body));
-    });
+var url2io = function(token, url = null, callback = null, fields = []) {
+    this.token = token;
+    this.handle = function(url, callback, fields = []) {
+        request({
+            method: 'GET',
+            uri: 'http://api.url2io.com/article',
+            qs: {
+                token: token,
+                url: url,
+                fields: fields.join(',')
+            }
+        },
+        function(error, res, body) {
+            callback(JSON.parse(body));
+        });
+    }
+    if (url!=null && callback!=null) {
+        this.handle(url, callback, fields);
+    }
 }
 
 module.exports = url2io;
